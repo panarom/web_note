3b54b1e4d149a760cde6b875e165ffd032df3aa5
Matt Panaro <matt.panaro@gmail.com>
2012-03-25 14:46:43 -0400

    modify javascript behavior to get note text when note link is clicked on from tag listing and hide it when link is clicked on again

--- public/javascript.js
+++ public/javascript.js
@@ -33,7 +33,12 @@ get_note_text=function()
 		{
 			var id_url = event.currentTarget.href;
 			event.preventDefault();
-			$('#' + id_url.split('/').pop()).load(id_url + ' #note_content');
+			var note_content_div_selector = $( '#' + id_url.split('/').pop() )
+			if ( note_content_div_selector.is(':empty') ) {
+					note_content_div_selector.load(id_url + ' #note_content');
+			} else {
+				note_content_div_selector.empty()
+			}
 		}
 	)
 }
